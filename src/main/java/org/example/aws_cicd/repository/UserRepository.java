ef3e8e98c6da26b8e769faa67517cf48abb2bb9c
myungji2 <myungji129@naver.com>
2024-06-26 15:23:09 +0900

    first commit

--- src/main/java/org/example/aws_cicd/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.aws_cicd.repository;
-
-import org.example.aws_cicd.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
